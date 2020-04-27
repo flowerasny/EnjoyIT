09779c407e682cdb9c6cde78c0add281ae9813d0
jakubkwiatek <jakub.kwiatek@azimo.com>
2020-04-27 20:25:30 +0200

    structure not working

--- EnjoyIT-App/ClientApp/src/App.vue
+++ EnjoyIT-App/ClientApp/src/App.vue
@@ -1,28 +1,120 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="projectsContainer">
+      <project-cards-list
+        v-for="status in statuses"
+        :key="status"
+        :status="status"
+        :projects="projects"
+        class="projectsList"
+      ></project-cards-list>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProjectCardList from "./components/ProjectCardList.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    ProjectCardList
+  },
+  data: {
+    projects: [
+      {
+        status: "IN_PROGRESS",
+        title:
+          "Kalendarz UEk pozwalający wyświetlić wydarzenia mające miejsce na uczelni",
+        counter: 32
+      },
+      {
+        status: "DONE",
+        title: "Aplikacja do zarządzania projektami studentów",
+        counter: 12
+      },
+      {
+        status: "TODO",
+        title: "Super sprawny system rezerwacji sal",
+        counter: 1
+      },
+      {
+        status: "TODO",
+        title:
+          "Inteligentna porównywarka kursów walut, która wychwytuje i podkreśla niespodziewane skoki i spadki cen",
+        counter: 1241
+      },
+      {
+        status: "DONE",
+        title:
+          "Kalendarz UEk pozwalający wyświetlić wydarzenia mające miejsce na uczelni",
+        counter: 32
+      },
+      {
+        status: "IN_PROGRESS",
+        title: "Aplikacja do zarządzania projektami studentów",
+        counter: 12
+      },
+      {
+        status: "TODO",
+        title: "Super sprawny system rezerwacji sal",
+        counter: 1
+      },
+      {
+        status: "IN_PROGRESS",
+        title:
+          "Inteligentna porównywarka kursów walut, która wychwytuje i podkreśla niespodziewane skoki i spadki cen",
+        counter: 1241
+      },
+      {
+        status: "DONE",
+        title:
+          "Kalendarz UEk pozwalający wyświetlić wydarzenia mające miejsce na uczelni",
+        counter: 32
+      },
+      {
+        status: "DONE",
+        title: "Aplikacja do zarządzania projektami studentów",
+        counter: 12
+      },
+      {
+        status: "IN_PROGRESS",
+        title: "Super sprawny system rezerwacji sal",
+        counter: 1
+      },
+      {
+        status: "DONE",
+        title:
+          "Inteligentna porównywarka kursów walut, która wychwytuje i podkreśla niespodziewane skoki i spadki cen",
+        counter: 1241
+      },
+      {
+        status: "TODO",
+        title:
+          "Kalendarz UEk pozwalający wyświetlić wydarzenia mające miejsce na uczelni",
+        counter: 32
+      },
+      {
+        status: "TODO",
+        title: "Aplikacja do zarządzania projektami studentów",
+        counter: 12
+      },
+      {
+        status: "TODO",
+        title: "Super sprawny system rezerwacji sal",
+        counter: 1
+      },
+      {
+        status: "DONE",
+        title:
+          "Inteligentna porównywarka kursów walut, która wychwytuje i podkreśla niespodziewane skoki i spadki cen",
+        counter: 1241
+      }
+    ],
+    statuses: ["TODO", "IN_PROGRESS", "DONE"]
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
